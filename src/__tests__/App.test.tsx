572cb5dca3b49c0a8660e9316b54368807ec6705
alvinlal <alvinzzz2001@gmail.com>
2022-09-24 22:17:07 +0530

    testing husky

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -5,7 +5,7 @@ describe('App.tsx', () => {
   it('displays hello world', () => {
     render(<App />);
     const helloWorldElement = screen.getByRole('heading', {
-      name: 'hello worl',
+      name: 'hello world',
     });
     expect(helloWorldElement).toBeInTheDocument();
   });
