4ad46ad443340a3a6d783e1a93fdda4ef32f409c
alvinlal <alvinzzz2001@gmail.com>
2022-09-24 19:43:01 +0530

    setup storybook

---
+++ storybook-static/assets/es.map.constructor.22a0ff0f.js
@@ -0,0 +1,2 @@
+import{n as o,o as t}from"./iframe.99cca877.js";var r=o,e=t;r("Map",function(n){return function(){return n(this,arguments.length?arguments[0]:void 0)}},e);
+//# sourceMappingURL=es.map.constructor.22a0ff0f.js.map
