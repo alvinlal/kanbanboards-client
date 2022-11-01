e295a89366795a6b1aca11e283c5962a943a790f
alvinlal <alvinzzz2001@gmail.com>
2022-11-01 23:04:35 +0530

    update search.ts

--- test-utils/msw/handlers/search.ts
+++ test-utils/msw/handlers/search.ts
@@ -3,7 +3,6 @@ import { apiEndPoint } from '../baseUrls';
 import searchResults from '../../mocks/searchResults.json';
 
 const searchHandlers = [
-  // migrate to get (remember to URLencode search query)
   rest.get(apiEndPoint('/search'), (req, res, ctx) => {
     return res(ctx.delay(300), ctx.json(searchResults));
   }),
