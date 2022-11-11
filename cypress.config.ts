a25941c427bee2e10a17a30f6b01af4677afad23
alvinlal <alvinzzz2001@gmail.com>
2022-11-11 15:06:16 +0530

    change port

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173',
+    baseUrl: 'http://localhost:4000',
     viewportWidth: 1920,
     viewportHeight: 1080,
   },
